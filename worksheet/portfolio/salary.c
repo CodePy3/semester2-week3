d448797e742a75220be802168ea5ddfd931fce3e
CodePy3 <jbmonks666@icloud.com>
2026-02-13 13:41:11 +0000

    Portfolio task completed

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -1,10 +1,11 @@
 
 #include <stdio.h>
+#include <math.h>
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Jonathan Monks
+ * ID: 201968903
  */
 
  int main( void ) {
@@ -18,6 +19,23 @@
     //printf("NI contribution £...",var_name);
     //printf("Tax contribution £...",var_name);
     //printf("Take home salary £...",var_name);
+   float salary = 36250;
+   float NI = 0.08;
+   float Tax = 0.15;
+
+   float NIdeduction = salary*NI;
+   float salaryMinusNI = salary-NIdeduction;
+
+   float taxableSalary = (float)salaryMinusNI-12500;
+   float taxContribution = taxableSalary*Tax;
+   float TakeawaySalary = salaryMinusNI-taxContribution;
+   
+   printf("Salary £%.2f\n",salary);
+   printf("NI contribution £%.2f\n",NIdeduction);
+   printf("Tax contribution £%.2f\n",taxContribution);
+   printf("Take home salary £%.2f\n",TakeawaySalary);
+
+
 
     return 0;
  }
